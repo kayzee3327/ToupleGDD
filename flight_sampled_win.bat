62d3e160222d4c8be6951218b5252647887c811e
王楷泽 <119168400+kayzee3327@users.noreply.github.com>
2025-05-20 13:12:48 +0800

    fix sampled exp

--- flight_sampled_win.bat
+++ flight_sampled_win.bat
@@ -3,10 +3,10 @@ python main.py --graph flight_delay_data\2016_中国_平均-150.csv --model Tripling
 python main.py --graph flight_delay_data\2016_中国_平均-200.csv --model Tripling --model_file tripling.ckpt --budget 150 --test --dismantle
 python main.py --graph flight_delay_data\2016_中国_平均-250.csv --model Tripling --model_file tripling.ckpt --budget 190 --test --dismantle
 python main.py --graph flight_delay_data\2016_中国_平均-300.csv --model Tripling --model_file tripling.ckpt --budget 225 --test --dismantle
-python main.py --graph flight_delay_data\2016_美国_平均-100.csv --model Tripling --model_file tripling.ckpt --budget 75 --test --dismantle
-python main.py --graph flight_delay_data\2016_美国_平均-150.csv --model Tripling --model_file tripling.ckpt --budget 100 --test --dismantle
-python main.py --graph flight_delay_data\2016_美国_平均-200.csv --model Tripling --model_file tripling.ckpt --budget 150 --test --dismantle
-python main.py --graph flight_delay_data\2016_美国_平均-250.csv --model Tripling --model_file tripling.ckpt --budget 190 --test --dismantle
-python main.py --graph flight_delay_data\2016_美国_平均-300.csv --model Tripling --model_file tripling.ckpt --budget 225 --test --dismantle
+python main.py --graph flight_delay_data\2017_美国_平均-100.csv --model Tripling --model_file tripling.ckpt --budget 75 --test --dismantle
+python main.py --graph flight_delay_data\2017_美国_平均-150.csv --model Tripling --model_file tripling.ckpt --budget 100 --test --dismantle
+python main.py --graph flight_delay_data\2017_美国_平均-200.csv --model Tripling --model_file tripling.ckpt --budget 150 --test --dismantle
+python main.py --graph flight_delay_data\2017_美国_平均-250.csv --model Tripling --model_file tripling.ckpt --budget 190 --test --dismantle
+python main.py --graph flight_delay_data\2017_美国_平均-300.csv --model Tripling --model_file tripling.ckpt --budget 225 --test --dismantle
 
     
